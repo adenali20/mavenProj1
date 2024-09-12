ae65b93bac7cd1ce4bb66d5401ab8167b824492c
adenali20 <adenali362@gmail.com>
2024-09-11 21:18:21 -0500

    init message

---
+++ src/main/java/com/example/mavenproj1/MavenProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.mavenproj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenProj1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mavenproj1/MavenProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenproj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProj1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
